a225cd73ceca150d216ea756e9308914af33bd2d
PeruLander <a.23308051281220@cbtis128.edu.mx>
2024-10-19 19:31:51 -0600

    Add files via upload

---
+++ pt2.py
@@ -0,0 +1,23 @@
+print("")
+print("Zamarripa Castro Erick Fabián 3W 1220")
+print("")
+
+#Crear diccionario vacio
+
+persona = {}
+
+nombre = input("Ingrese su nombre: ")
+persona["nombre: "] = nombre
+print(persona)
+edad = input("Ingrese su edad: ")
+persona["edad: "] = edad
+print(persona)
+sexo = input("Ingrese su sexo: ")
+persona["sexo: "] = sexo
+print(persona)
+telefono = input("Ingrese su numero de telefono: ")
+persona["telefono: "] = telefono
+print(persona)
+correo = input("Ingrese su correo electronico: ")
+persona["correo: "] = correo
+print(persona)
